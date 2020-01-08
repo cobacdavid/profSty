073c6904c6f277dda7f89323ee6369319aee9e96
cobacdavid <7426305+cobacdavid@users.noreply.github.com>
2020-01-08 18:17:50 +0100

    html

--- auto/profPython.el
+++ auto/profPython.el
@@ -2,10 +2,17 @@
  "profPython"
  (lambda ()
    (TeX-add-symbols
-    '("vpythondirect" 1)
+    '("vcss" 1)
+    '("vueSite" 1)
+    '("vhtml" 1)
     '("vpython" 1)
     "python")
+   (LaTeX-add-environments
+    "pythondirect"
+    "htmldirect")
    (LaTeX-add-lengths
-    "pythonLogoSize"))
+    "pythonLogoSize"
+    "htmlLogoSize"
+    "cssLogoSize"))
  :latex)
 
