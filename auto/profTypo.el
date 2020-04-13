c80163d9afd0a208f6ffe4f10a5f045d887d87c3
cobacdavid <7426305+cobacdavid@users.noreply.github.com>
2020-04-13 18:04:21 +0200

    blocTexte

--- auto/profTypo.el
+++ auto/profTypo.el
@@ -17,6 +17,9 @@
     "RRoman")
    (LaTeX-add-environments
     '("commentaire" LaTeX-env-args ["argument"] 1)
-    '("monbloc" LaTeX-env-args ["argument"] 1)))
+    '("monbloc" LaTeX-env-args ["argument"] 1)
+    '("monbloc" LaTeX-env-args ["argument"] 3)
+    '("monblocnb" LaTeX-env-args ["argument"] 2)
+    '("monblocb" LaTeX-env-args ["argument"] 2)))
  :latex)
 
